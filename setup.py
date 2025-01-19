df3f9cfa5b1fc61985678b2747287067c585f206
Lars Løberg Monstad <lars.monstad@gmail.com>
2025-01-19 21:01:50 +0100

    fixed 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="amt-augpy",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=[
         'librosa==0.9.1',  # Specific version for compatibility with torchcrepe
